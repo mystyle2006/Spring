a99608c23c3d21495fba57f859ce8790a4dff321
INHOLEE <sayyou0918@gmail.com>
2017-11-16 14:11:37 +0900

    Form Data Process

---
+++ 08 Form Data/src/main/java/com/java/member/Member.java
@@ -0,0 +1,33 @@
+package com.java.member;
+
+public class Member {
+	String username;
+	String id;
+	String pw;
+	String email;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
