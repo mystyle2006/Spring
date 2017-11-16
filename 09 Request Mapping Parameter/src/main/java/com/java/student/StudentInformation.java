1daf652085afd7a96d51ca2aa63e8f9c7b6c2348
INHOLEE <sayyou0918@gmail.com>
2017-11-16 14:47:40 +0900

    Request Mapping Paramter by using form tag

---
+++ 09 Request Mapping Parameter/src/main/java/com/java/student/StudentInformation.java
@@ -0,0 +1,19 @@
+package com.java.student;
+
+public class StudentInformation {
+	private String name;
+	private int grade;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+}
